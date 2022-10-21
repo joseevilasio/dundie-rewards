a8b00a8c92fe736a3fa50d4151cba7e1aa7a292f
José Junior <61568510+joseevilasio@users.noreply.github.com>
2022-10-21 09:20:31 -0300

    Issue 9 (#12)
    * uptade
    
    * Set up project initial organization and metadata
    
    * Fixed newline
    
    close #8
    
    * turned to installable binary #9

--- dundie/__main__.py
+++ dundie/__main__.py
@@ -1,1 +1,3 @@
-print("Executing entry point for dundie...")
+
+def main():
+    print("Executing dundie from entry point.")
--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup, find_packages
 
-
 setup(
     name="dundie",
     version="0.1.0",
     description="Reward Point System for Dunder Mifflin",
     author="Jose Junior",
     packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "dundie = dundie.__main__:main"
+        ]    
+    }
 )
