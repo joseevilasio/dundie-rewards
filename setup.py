cd35492b5482a8628e4a2ee048f29aa980422dd0
joseevilasio <jose_evilasio@outlook.com>
2022-10-20 21:15:16 -0300

    Set up project initial organization and metadata

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="dundie",
+    version="0.1.0",
+    description="Reward Point System for Dunder Mifflin",
+    author="Jose Junior",
+    packages=find_packages(),
+)
