cc3d59841af37eebf2f27ba7b2026f4a9e619454
joseevilasio <jose_evilasio@outlook.com>
2022-11-23 20:38:43 -0300

    add test add/remove points

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,5 +1,5 @@
 import pytest
-from dundie.database import EMPTY_DB, connect, commit, add_person
+from dundie.database import EMPTY_DB, connect, commit, add_person, add_movement
 
 
 @pytest.mark.unit
@@ -21,7 +21,6 @@ def test_commit_to_database():
 
 
 @pytest.mark.unit
-@pytest.mark.low
 def test_add_person_for_the_first_time():
     pk = "joe@doe.com"
     data = {"name": "Joe Doe", "role": "Salesman", "dept": "Sales"}
@@ -41,3 +40,26 @@ def test_add_person_for_the_first_time():
 def test_negative_add_person_invalid_email():
     with pytest.raises(ValueError):
         add_person({}, ".@bla", {})
+
+
+@pytest.mark.unit
+@pytest.mark.low
+def test_add_or_remove_points_for_person():
+    pk = "joe@doe.com"
+    data = {"name": "Joe Doe", "role": "Salesman", "dept": "Sales"}
+    db = connect()
+    _, created = add_person(db, pk, data)
+    assert created is True
+    commit(db)
+    
+    db = connect()
+    before = db["balance"][pk]
+    add_movement(db, pk, -100, "manager")
+    commit(db)
+
+    db = connect()
+    after = db["balance"][pk]
+
+    assert after == before - 100
+    assert before == 500
+    assert after == 400
