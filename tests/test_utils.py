6efb9a3440c80eeafd2c3fb4f803f1a1fdb81454
joseevilasio <jose_evilasio@outlook.com>
2023-01-13 21:04:53 -0300

    add test new validations

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,6 +2,10 @@ import pytest
 
 from dundie.utils.email import check_valid_email
 from dundie.utils.user import generate_simple_password
+from dundie.database import get_session
+from dundie.models import Person, InvalidEmailError
+from dundie.utils.db import add_person
+from dundie.utils.login import validation_user_if_exist
 
 
 @pytest.mark.unit
@@ -33,3 +37,70 @@ def test_generate_simple_password():
         passwords.append(generate_simple_password(8))
 
     assert len(set(passwords)) == 100
+
+
+@pytest.mark.unit
+@pytest.mark.parametrize("user", ["joe@doe.com", "jim@doe.com"])
+def test_positive_validation_user_if_exist(user):
+    """Ensure user is valid"""
+    with get_session() as session:
+
+        joe = {
+            "email": "joe@doe.com",
+            "name": "Joe Doe",
+            "dept": "Sales",
+            "role": "Salesman",
+        }
+
+        instance_joe = Person(**joe)
+        _, created = add_person(session=session, instance=instance_joe)
+        assert created is True
+
+        jim = {
+            "email": "jim@doe.com",
+            "name": "Jim Doe",
+            "dept": "Management",
+            "role": "Manager",
+        }
+
+        instance_jim = Person(**jim)
+        _, created = add_person(session=session, instance=instance_jim)
+        assert created is True
+
+        session.commit()
+        
+        assert validation_user_if_exist(user) is True      
+
+
+@pytest.mark.unit
+@pytest.mark.parametrize("user", ["flavio@doe.com", "jose@doe.com"])
+def test_negative_validation_user_if_exist(user):
+    """Ensure user is valid"""
+    with pytest.raises(InvalidEmailError):
+        with get_session() as session:
+
+            joe = {
+                "email": "joe@doe.com",
+                "name": "Joe Doe",
+                "dept": "Sales",
+                "role": "Salesman",
+            }
+
+            instance_joe = Person(**joe)
+            _, created = add_person(session=session, instance=instance_joe)
+            assert created is True
+
+            jim = {
+                "email": "jim@doe.com",
+                "name": "Jim Doe",
+                "dept": "Management",
+                "role": "Manager",
+            }
+
+            instance_jim = Person(**jim)
+            _, created = add_person(session=session, instance=instance_jim)
+            assert created is True
+
+            session.commit()
+            
+            validation_user_if_exist(user)
