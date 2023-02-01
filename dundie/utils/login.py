7722edc440008bf60712de0e6381e25cf940243f
joseevilasio <jose_evilasio@outlook.com>
2023-02-01 13:16:20 -0300

    add new function and upgrade function require pass

--- dundie/utils/login.py
+++ dundie/utils/login.py
@@ -3,6 +3,7 @@ import os
 
 from sqlmodel import select
 
+from dundie import core
 from dundie.database import get_session
 from dundie.models import InvalidEmailError, Person, User
 from dundie.settings import DUNDIE_ADMIN_USER, DUNDIE_ADMIN_USER_PASSWORD
@@ -71,32 +72,87 @@ def validation_password(user: str, password: str) -> bool:
             raise InvalidPasswordError(f"Invalid password for {user!r}")
 
 
-def require_password() -> bool:
+def require_password(admin_only: bool) -> bool:
     """Input user and password"""
 
     user = os.getenv("DUNDIE_USER")
     password = os.getenv("DUNDIE_PASSWORD")
 
-    if user and password:
-        if validation_user_if_exist(user):
-            if validation_password(user, password):
-                return True
+    if not admin_only:
 
-    elif user and not password:
-        if validation_user_if_exist(user):
-            password = getpass.getpass()
-            if validation_password(user, password):
-                os.environ["DUNDIE_PASSWORD"] = password
-                return True
+        if user and password:
+            if validation_user_if_exist(user):
+                if validation_password(user, password):
+                    return True
 
-    elif not user:
-        user = input(str("User: "))
-        if validation_user_if_exist(user):
-            password = getpass.getpass()
-            if validation_password(user, password):
-                os.environ["DUNDIE_USER"] = user
-                os.environ["DUNDIE_PASSWORD"] = password
-                return True
+        if user and not password:
+            if validation_user_if_exist(user):
+                password = getpass.getpass()
+                if validation_password(user, password):
+                    os.environ["DUNDIE_PASSWORD"] = password
+                    return True
 
+        if not user:
+            user = input(str("User: "))
+            if validation_user_if_exist(user):
+                password = getpass.getpass()
+                if validation_password(user, password):
+                    os.environ["DUNDIE_USER"] = user
+                    os.environ["DUNDIE_PASSWORD"] = password
+                    return True
+
+        else:
+            raise AccessDeniedError("⚠️ Access Denied ⚠️")
+
+    elif admin_only:
+
+        if user and password:
+            if user == DUNDIE_ADMIN_USER:
+                if password == DUNDIE_ADMIN_USER_PASSWORD:
+                    return True
+
+        if user and not password:
+            if user == DUNDIE_ADMIN_USER:
+                password = getpass.getpass()
+                if password == DUNDIE_ADMIN_USER_PASSWORD:
+                    os.environ["DUNDIE_PASSWORD"] = password
+                    return True
+
+        if not user:
+            user = input(str("User: "))
+            if user == DUNDIE_ADMIN_USER:
+                password = getpass.getpass()
+                if password == DUNDIE_ADMIN_USER_PASSWORD:
+                    os.environ["DUNDIE_USER"] = user
+                    os.environ["DUNDIE_PASSWORD"] = password
+                    return True
+
+        else:
+            raise AccessDeniedError("⚠️ Access Denied ⚠️")
+
+
+def handles_query_for_user(**query):
+    """Manipulates read data from db and filters using query
+
+    read(email="joe@doe.com")
+    """
+    query = {k: v for k, v in query.items() if v is not None}
+
+    user = os.getenv("DUNDIE_USER")
+
+    with get_session() as session:
+
+        role = session.exec(
+            select(Person.role).where(Person.email == user)
+        ).first()
+        dept = session.exec(
+            select(Person.dept).where(Person.email == user)
+        ).first()
+
+    if user == DUNDIE_ADMIN_USER:
+        return core.read(**query)
     else:
-        return False
+        if role == "Manager":
+            return core.read(dept=dept)
+        else:
+            return core.read(email=user)
