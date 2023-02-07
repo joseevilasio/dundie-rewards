1a2ae35ba219f3ed44181d7209f925f660b8ca45
joseevilasio <jose_evilasio@outlook.com>
2023-02-06 23:20:40 -0300

    convert for encrypto

--- integration/constants.py
+++ integration/constants.py
@@ -5,4 +5,5 @@ PEOPLE_FILE = os.path.join(TEST_PATH, "assets/people.csv")
 # os.path.join(TEST_PATH, "../tests", "assets/people.csv")
 
 DUNDIE_ADMIN_USER: str = "michael@dundermifflin.com"
-DUNDIE_ADMIN_USER_PASSWORD: str = "qW34eRT9"
+DUNDIE_ADMIN_USER_PASSWORD: str = """gAAAAABj4a-ksOTbTEvQmMh6Gjr9tCFv4u_
+Sis0FnMiBh6ahbESAL3qb_VFn-ZNxVpcfXOJ51B5JCKXNaJ36yeyd9_3WrG0zZA=="""
